6bad220835d451de2c5f8887aefcd92df28bf876
Isayas <60773892+isayasone@users.noreply.github.com>
2023-12-14 12:47:45 +0000

    feat:prove to the server we're on the nice list

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const { keccak256 } = require('ethereum-cryptography/keccak');
 const verifyProof = require('../utils/verifyProof');
-
+const niceList= require('../utils/niceList.json');
+const  MerkleTree= require('../utils/MerkleTree')
+const  merkleTree= new  MerkleTree(niceList)
 const port = 1225;
-
 const app = express();
 app.use(express.json());
-
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
-
+const MERKLE_ROOT =  merkleTree.getRoot();
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
+  const {proof,  claimedIdentity}=body;
+  console.log(proof)
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof( proof,  claimedIdentity,MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
